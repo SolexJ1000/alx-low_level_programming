a1c009c25d1df5f0e0fed7e5359553dea337d0f6
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-09 09:04:44 -0700

    updated script 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- main - Entry point
+* main - Entry point
 *
-* Return: Always (0)
+* Retuns: Always (0) 
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
-}
+}	
