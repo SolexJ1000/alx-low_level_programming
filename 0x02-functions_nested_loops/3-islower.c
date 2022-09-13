11c8b6d4298611bf5f6e9d744aca2b598556ae6b
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-13 12:34:48 -0700

    Created progran 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _islower - function that checks for lowercase character.
+*
+* Description: A function that checks for lowercase character.
+* Returns 1 if c is lowercase, Returns 0 otherwise
+*
+* Return: 1 if number is lowercase and 0 if number is uppercase
+*/
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	
+	{
+		if (i == c)
+		{
+			lower = 1;
+		}
+	}
+	return (lower);
+}
