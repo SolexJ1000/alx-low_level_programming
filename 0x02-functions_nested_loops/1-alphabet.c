cce7b7163548644812ab918bc11cd610698da468
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-13 11:48:45 -0700

    Updated script 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - prints the alphabet, in lowercase
+* print_alphabet - prints the alphabet, in lowercase
 *
 * Description: a function that prints the alphabet, in lowercase,
 * followed by a new line
