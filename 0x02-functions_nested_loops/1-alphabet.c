fd14ad6e3f9191b949f40f7679502b918f2cdc40
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-13 11:43:40 -0700

    Created program 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - prints the alphabet, in lowercase
+*
+* Description: a function that prints the alphabet, in lowercase,
+* followed by a new line
+*
+* Return: 0 Always
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c='a'; c<='z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
