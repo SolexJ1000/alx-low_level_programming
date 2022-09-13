70dac20fbbe024b6daf55d8b4fcba83086628777
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-13 10:48:44 -0700

    updated program 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 /**
 * main - program that prints _putchar
 * Description: This program that prints _putchar foolowed
@@ -8,13 +8,13 @@
 */
 int main(void)
 {
-	char word89] = "_putchar";
+	char word[8] = "_putchar";
 	int a;
 
-	for (a=0; a<8; a++)
+	for (a = 0; a < 8; a++)
 	{
 		_putchar(word[a]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
