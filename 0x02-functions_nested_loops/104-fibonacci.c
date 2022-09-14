4f4b6c6a7b945f87b4af7272e1460de0e4588354
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-13 22:10:30 -0700

    updated program 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -29,7 +29,8 @@ int numLength(int num)
 *  Return: 0
 */
 int main(void)
-{	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+{	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0,
+	f2o = 0, tmpo = 0;
 	short int i = 1, initial0s;
 
 	while (i <= 98)
