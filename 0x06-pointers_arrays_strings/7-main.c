6a7ef071da4f5c9ac7fbc7e854be02b95c3cf8a3
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-22 20:56:32 -0700

    updated program 7

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -6,8 +6,8 @@
 */
 int main(void)
 {
-  char s[] = "Expect the best. Prepare for the worst."
-	  "Capitalize on what comes.\n";
+  char s[] = "Expect the best. Prepare for the worst.
+	  Capitalize on what comes.\n";
   char *p;
 
   p = leet(s);
