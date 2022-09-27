c1c9564f47d53af2fd36d6110b53f002fe2b1175
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-27 10:26:12 -0700

    updated function 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,10 +13,11 @@ Return: Pointer to location filled
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *start = s;
+
 	while (n--)
 	{
 		*s = b;
 		s++;
 	}
-	return(start);
+	return (start);
 }
