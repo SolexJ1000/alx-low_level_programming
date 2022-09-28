3015473a6ada147fa1304978efc901ded54b5ad6
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-28 10:54:54 -0700

    Created funtion 00

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* void _puts_recursion - a function that prints a string
+* and a new line
+*
+* @s: The  input string to print
+*
+* Return: The string inputed
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory contains files and functions demonstrating recursions in C
