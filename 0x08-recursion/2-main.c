d4f7d6790a5b18cef136ba8afddd591b5ce41b7f
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-28 19:01:17 -0700

    Created function 02

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - calls other functions
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("I am trying my best");
+	printf("%d\n", n);
+	return (0);
+}
