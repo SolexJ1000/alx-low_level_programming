1717eae03dca8cd9d70b3800670575cee2647116
SolexJ1000 <joseph.appolos60@gmail.com>
2022-09-30 13:06:38 -0700

    Updated function 00

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,15 @@
  * @argc: counts the arguments passed in the function
  * @argv: lists the argument passed
  *
- * Return: Always 
+ * Return: Always
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("The name of the program is %s\n", argv[0]);
+
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
