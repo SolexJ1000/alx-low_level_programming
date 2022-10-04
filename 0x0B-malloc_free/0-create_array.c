60ce74a66c089e1a97ee8578f014df7f572a7c2e
SolexJ1000 <joseph.appolos60@gmail.com>
2022-10-04 10:36:04 -0700

    updated function 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc(size * sizeof(char));
+	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 	{
 		return (NULL);
