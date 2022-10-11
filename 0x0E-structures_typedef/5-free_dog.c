d831bd99ed2fbf874d437a40340d04f52e1145c9
SolexJ1000 <joseph.appolos60@gmail.com>
2022-10-10 19:03:46 -0700

    Created function 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - A function that frees structs
+ *
+ * @dog_t: the first struct
+ * @d: the second struct
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
