36b1243ac7997261ce0b82467ae9ada272f9242e
SolexJ1000 <joseph.appolos60@gmail.com>
2022-10-10 22:53:45 -0700

    Updated function 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024.
+ */
+#define SIZE (1024)
+#endif /* _OBJECT_LIKE_MACRO_H_ */
