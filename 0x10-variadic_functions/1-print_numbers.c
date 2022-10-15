21221db8e4656cb7c918d4f2c96fb4b44991d7ca
SolexJ1000 <joseph.appolos60@gmail.com>
2022-10-15 10:37:42 -0700

    created function 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers passed to it as parameters
+ *
+ * @seperator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ *@...: other parameters
+ *
+ * Return: the printed numbers
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list ch;
+	unsigned int i;
+	int numbs;
+
+	va_start(ch, n);
+	for (i = 0; i < n; i++)
+	{
+		numbs = va_arg(ch, int);
+		printf("%d", numbs);
+			continue;
+		if (i < n - 1)
+		{
+			printf("%s", seperator);
+		}
+	}
+	printf("\n");
+	va_end(ch);
+}
